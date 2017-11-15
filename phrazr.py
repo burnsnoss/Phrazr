fa42d9b0b7ffe0c2e508b3e7d1155bff54f03ed7
Casey Burns <cburns@ifarm-dev3.agintegrated.com>
2017-11-15 17:13:59 -0500

    added Casey + Ty phrazebook and streamlined logic.

--- phrazr.py
+++ phrazr.py
@@ -2,24 +2,28 @@ import random
 import sys
 
 SCRUM_PHRAZEBOOK_PATH = './ScrumPhrazebook.txt'
+CASEY_PHRAZEBOOK_PATH = './CaseyPhrazebook.txt'
+TY_PHRAZEBOOK_PATH = './TyPhrazebook.txt'
+
+options = ['-casey', '-scrum', '-ty']
 
 def displayError(errorType):
 	print 'ERROR'
 	print 'CODE: 69'
 	if errorType == 'runtime':
 		print 'How to run Phrazr:'
-		print '~$ python phrazr.py scrum'
-		print '\nRight now, we only have one option, "scrum". Deal with it.'
-		print 'run ~$ "python phrazr.py help" for more help.'
+		print '~$ python phrazr.py -option'
+		print '\nRight now, we only have two options, "-scrum" and "-casey". Deal with it.'
+		print 'run ~$ "python phrazr.py -help" for more help.'
 	elif errorType == 'arguments':
-		print 'Please provide "scrum" as an argument.'
+		print 'Please provide "-scrum" or "-casey" as an argument.'
 	return  
 
 def printHelp():
 	print '\nHow to run Phrazr:'
-	print '~$ python phrazr.py scrum'
-	print '"scrum" is currently the only option we support.'
-	print '\nCopyright BonerJamz Inc. - Written by McFarts.' 
+	print '~$ python phrazr.py -option'
+	print '"-scrum" and "-casey" are currently the only options we support.'
+	print '\nCopyright BonerJamz Inc. 2017 - Written by McFarts.' 
 	return
 
 def parseInput(argv):
@@ -27,9 +31,9 @@ def parseInput(argv):
 		displayError('runtime')
 		exit()
 	else:
-		if argv[1] == 'scrum':
-			return 'scrum'
-		elif argv[1] == 'help':
+		if argv[1] in  options:
+			return argv[1]
+		elif argv[1] == '-help':
 			printHelp()
 			exit()
 		else:
@@ -37,17 +41,24 @@ def parseInput(argv):
 			exit()
 
 def generatePhraze(cmd):
-	if cmd == 'scrum':
-		phraze = getScrumPhraze()
-		print phraze
-		exit()
+	if cmd == '-scrum':
+		phraze = getPhraze(SCRUM_PHRAZEBOOK_PATH)
+	elif cmd == '-casey':
+		phraze = getPhraze(CASEY_PHRAZEBOOK_PATH)
+	elif cmd == '-ty':
+		phraze = getPhraze(TY_PHRAZEBOOK_PATH)
 	else:
 		displayError('runtime')
+		exit()
+	print phraze
+	return
 
-def getScrumPhraze():
-	filestream = open(SCRUM_PHRAZEBOOK_PATH, 'r')
-	phrazes = filestream.read().split('\n')
-	idx = random.randint(0, len(phrazes))
+def getPhraze(phrazebook):
+	filestream = open(phrazebook, 'r')
+	phrazes = filestream.read().strip('\n').split('\n')
+	idx = random.randint(0, len(phrazes) - 1)
+	print 'Index: ', idx
+	print '#Phrazes: ', len(phrazes)
 	return phrazes[idx]
 
 if __name__ == '__main__':
