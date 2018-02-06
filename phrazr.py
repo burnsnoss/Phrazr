933ed584318e0ceb0e91cced428b397670a146e4
Casey Burns <cburns@agintegrated.com>
2018-02-06 15:57:10 -0500

    added nathans dad phrazebook and random ability.

--- phrazr.py
+++ phrazr.py
@@ -1,16 +1,23 @@
 import random
 import sys
 
+# TO DO: read these paths in by looking for files with regex *Phrazebook.txt
+#  Then how to do options?
+
 SCRUM_PHRAZEBOOK_PATH = './ScrumPhrazebook.txt'
 CASEY_PHRAZEBOOK_PATH = './CaseyPhrazebook.txt'
 TY_PHRAZEBOOK_PATH = './TyPhrazebook.txt'
 TEAM_PHRAZEBOOK_PATH = './TeamPhrazebook.txt'
+RANDOM_PHRAZEBOOK_PATH = './RandomPhrazebook.txt'
+NATHANSDAD_PHRAZEBOOK_PATH = './NathansDadPhrazebook.txt'
 
 options = {
 	'-casey': CASEY_PHRAZEBOOK_PATH,
 	'-scrum': SCRUM_PHRAZEBOOK_PATH,
 	'-ty': TY_PHRAZEBOOK_PATH,
-	'-team': TEAM_PHRAZEBOOK_PATH
+	'-team': TEAM_PHRAZEBOOK_PATH,
+	'-random': RANDOM_PHRAZEBOOK_PATH,
+	'-nathansdad': NATHANSDAD_PHRAZEBOOK_PATH
 }
 
 def displayError(errorType):
@@ -20,6 +27,8 @@ def displayError(errorType):
 		print 'Improper number of command line arguments. Shame on you.'
 	elif errorType == 'arguments':
 		print 'Invalid command line option.'
+	elif errorType == 'agglength':
+		print 'Invalid aggregate length.'
 	printHelp()
 
 	return  
@@ -29,7 +38,7 @@ def printHelp():
 	print '~$ python phrazr.py -option'
 	print 'Currently, we support these options:'
 	print ''
-	for opt in option:
+	for opt in options:
 		print '\t', opt
 	print '\nCopyright BonerJamz Inc. 2017 - Written by McFarts.' 
 	return
@@ -39,7 +48,7 @@ def parseInput(argv):
 		displayError('runtime')
 		exit()
 	else:
-		if argv[1] in options.keys():
+		if argv[1] in options.keys() or argv[1] == '-aggregate':
 			return argv[1]
 		elif argv[1] == '-help':
 			printHelp()
@@ -59,7 +68,35 @@ def getPhraze(phrazebook):
 	idx = random.randint(0, len(phrazes) - 1)
 	return phrazes[idx]
 
+def generateAggregatePhraze(length):
+	hashnum = 9437
+	aggbook = []
+	for opt in options:
+		filestream = open(options[opt], 'r')
+		phrazes = filestream.read().strip('\n').split('\n')
+		for i in range(len(phrazes)):
+			aggbook.append(phrazes[i].split(' '))
+		phrazes = []
+	
+	phraze = ''
+	for j in range(length):
+		idx1 = ((random.randint(0, len(aggbook) - 1) + random.randint(0, 10000)) * hashnum) % len(aggbook)
+		idx2 = ((random.randint(0, len(aggbook[idx1]) - 1) + random.randint(0, 10000)) * hashnum) % len(aggbook[idx1])
+		phraze += aggbook[idx1][idx2] + ' '
+
+	print phraze
+		
+		
+
 if __name__ == '__main__':
 	cmd = parseInput(sys.argv)
-	generatePhraze(cmd)
+	if cmd == '-aggregate':
+		phraze_length = raw_input('Length of phraze? [1-12] --> ')
+		if phraze_length.isdigit() and int(phraze_length) > 0 and int(phraze_length) <= 12:
+			generateAggregatePhraze(int(phraze_length))
+		else:
+			printError('agglength')
+			exit()
+	else:
+		generatePhraze(cmd)
 	exit()
